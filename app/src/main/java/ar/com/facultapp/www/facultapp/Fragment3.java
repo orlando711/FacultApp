d23a0c6231bae57d8370cd88179ff35ea878c586
Gaston Alvarez <gastonbc.ga@gmail.com>
2017-03-09 21:51:00 -0300

    Corregidos, 3 explicativos completos

--- app/src/main/java/ar/com/facultapp/www/facultapp/Fragment3.java
+++ app/src/main/java/ar/com/facultapp/www/facultapp/Fragment3.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link explicativo_1.OnFragmentInteractionListener} interface
+ * {@link Fragment3.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link explicativo_1#newInstance} factory method to
+ * Use the {@link Fragment3#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class explicativo_1 extends Fragment {
+public class Fragment3 extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +29,7 @@ public class explicativo_1 extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public explicativo_1() {
+    public Fragment3() {
         // Required empty public constructor
     }
 
@@ -39,11 +39,11 @@ public class explicativo_1 extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment explicativo_1.
+     * @return A new instance of fragment Fragment3.
      */
     // TODO: Rename and change types and number of parameters
-    public static explicativo_1 newInstance(String param1, String param2) {
-        explicativo_1 fragment = new explicativo_1();
+    public static Fragment3 newInstance(String param1, String param2) {
+        Fragment3 fragment = new Fragment3();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,7 +64,7 @@ public class explicativo_1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explicativo_1, container, false);
+        return inflater.inflate(R.layout.fragment_fragment3, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
